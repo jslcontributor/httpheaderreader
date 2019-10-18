c165371d5c495a3bd66194b90b0742017dde3859
root <root@DESKTOP-RHTCNO1.localdomain>
2019-10-17 22:47:51 -0700

    Commit, need to implement hash table to improve search

---
+++ hashTable.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LENGTH 30
+
+struct Node{
+    char *value;
+};
+
+struct HashTable{
+    struct Node *hashArray[LENGTH];
+    int size = LENGTH;
+};
+
+int hashIndex(char *key) {
+
+
+}
+
+st
